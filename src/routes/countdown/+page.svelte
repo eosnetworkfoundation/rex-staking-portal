947c88831da6f2a8812e760867dd08784605b1d0
nsjames <DENIED>
2024-07-05 16:20:42 +0100

    add static site generation

--- src/routes/countdown/+page.svelte
+++ src/routes/countdown/+page.svelte
@@ -2,6 +2,15 @@
     import CountdownTimer from "$lib/components/CountdownTimer.svelte";
     import BackgroundOverlay from "$lib/components/BackgroundOverlay.svelte";
     import BlurryBg from "$lib/components/BlurryBg.svelte";
+    import {onMount} from "svelte";
+    import {isLive} from "$lib";
+    import {goto} from "$app/navigation";
+
+    onMount(() => {
+        if(isLive()){
+            goto('/')
+        }
+    })
 </script>
 
 <section class="bg-black w-full min-h-screen relative overflow-x-hidden px-2">
